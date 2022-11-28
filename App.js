2d2ff5a496494b3cdd365c8c5380987666c44dcf
AMS <areebamuhammadsaeed@gmail.com>
2022-11-28 14:50:33 +0500

    first commit

--- App.js
+++ App.js
@@ -1,7 +1,62 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Alert } from 'react-native';
+import React, {useEffect} from 'react';
+import messaging from "@react-native-firebase/messaging";
 
 export default function App() {
+ 
+  const requestUserPermission = async() => {
+    const authStatus = await messaging().requestPermission();
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+    if (enabled) {
+      console.log('Authorization status:', authStatus);
+  }
+}
+useEffect(() => {
+  if(requestUserPermission()){
+    messaging().getToken().then(token => {
+     console.log(token); 
+    });
+
+  }
+  else{
+    console.log("Failed",authStatus);
+  }
+  messaging()
+    .getInitialNotification()
+    .then(async(remoteMessage) => {
+      if (remoteMessage) {
+        console.log(
+          'Notification caused app to open from quit state:',
+          remoteMessage.notification,
+          );
+        }
+      });
+  messaging().onNotificationOpenedApp(async (remoteMessage) => {
+      console.log(
+        'Notification caused app to open from background state:',
+        remoteMessage.notification,
+      );
+    });
+
+  messaging().setBackgroundMessageHandler(async remoteMessage => {
+      console.log('Message handled in the background!', remoteMessage);
+    });
+
+  const unsubscribe = messaging().onMessage(async remoteMessage => {
+      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+    });
+
+  return unsubscribe;
+    
+      
+
+
+},[])
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -9,6 +64,7 @@ export default function App() {
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
